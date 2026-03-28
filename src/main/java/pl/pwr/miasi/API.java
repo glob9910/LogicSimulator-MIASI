c1fc189aee8527437a56a34b11ba359dea1ab788
Kamila Jablonska <glob9910@o2.pl>
2026-03-28 19:41:14 +0100

    Add parsing 'ls' code to json. Add parsing function to API.

--- src/main/java/pl/pwr/miasi/API.java
+++ src/main/java/pl/pwr/miasi/API.java
@@ -1,7 +1,7 @@
 package pl.pwr.miasi;
 
 public class API {
-    public static String getMessage() {
-        return "hello from java";
+    public static String parse(String program) {
+        return new VisitorInitializer().initialize(program);
     }
 }
